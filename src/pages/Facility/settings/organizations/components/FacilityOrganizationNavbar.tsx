ed6f2f70925a552be48e48943f4230819e8ff70c
StephZam <stephysam008@gmail.com>
2025-08-26 17:02:29 +0530

    added resizable feature in dept/team

--- src/pages/Facility/settings/organizations/components/FacilityOrganizationNavbar.tsx
+++ src/pages/Facility/settings/organizations/components/FacilityOrganizationNavbar.tsx
@@ -139,7 +139,7 @@ export default function FacilityOrganizationNavbar({
   const topLevelOrganizations = allOrganizations?.results || [];
 
   return (
-    <div className="w-64 shadow-lg bg-white rounded-lg hidden md:block">
+    <div className="min-w-64 h-full shadow-lg bg-white rounded-lg hidden md:block">
       <ScrollArea className="h-[calc(100vh-14rem)]">
         <div className="p-4">
           {isLoadingOrganizations ? (
